3485fbdddd34feaa3494907c4ab9cf5d17069914
canronglan <16386862+canronglan@users.noreply.github.com>
2024-12-20 00:32:49 +0800

    Update update.sh

--- cert/update.sh
+++ cert/update.sh
@@ -1,3 +1,3 @@
 mkdir -p /root/cert
-wget -O $(dirname $(realpath "$0"))/fullchain.pem http://hktb2.biaotee.top/cert/fullchain.pem
-wget -O $(dirname $(realpath "$0"))/privkey.pem http://hktb2.biaotee.top/cert/privkey.pem
+wget -O $(dirname $(realpath "$0"))/fullchain.pem http://hktb2.biaotee.top/cert/fullchain.pem > /dev/null 2>&1 &
+wget -O $(dirname $(realpath "$0"))/privkey.pem http://hktb2.biaotee.top/cert/privkey.pem > /dev/null 2>&1 &
