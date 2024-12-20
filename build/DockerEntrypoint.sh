fa29044d3f659b1f5e27a21b65eb976461e46212
canronglan <16386862+canronglan@users.noreply.github.com>
2024-12-20 17:40:24 +0800

    Update DockerEntrypoint.sh
    添加 TINY_SUB 子网可选项

--- build/DockerEntrypoint.sh
+++ build/DockerEntrypoint.sh
@@ -15,7 +15,7 @@ fail2ban-client -x start
 # n2n edge
 /app/edge -I $HOST -l hktb2.biaotee.top:1000 -l hktb1.biaotee.top:1000 -c biaotee -k biaotee -a 192.168.254.$ID -r -E -d n2n0 >/dev/null 2>&1 &
 # tinyvpn
-/app/tinyvpn -s -l 0.0.0.0:65000 --sub-net 192.168.125.0 --tun-dev tiny0 >/dev/null 2>&1 &
+/app/tinyvpn -s -l 0.0.0.0:65000 --sub-net $TINY_SUB --tun-dev tiny0 >/dev/null 2>&1 &
 # easytier
 /app/easytier-core -i 192.168.124.$ID --dev-name easy0 --network-name biaotee --network-secret alan --hostname $HOST -p tcp://hktb2.biaotee.top:11015 tcp://hktb1.biaotee.top:11015 -l udp://[::]:11015 tcp://[::]:11015 tcp://0.0.0.0:11015 udp://0.0.0.0:11015 wg://0.0.0.0:11016 wg://[::]:11016 --latency-first >/dev/null 2>&1 &
 # Run x-ui
